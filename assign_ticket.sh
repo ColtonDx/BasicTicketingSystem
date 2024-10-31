1d4f347e5f1e3faad713c3df0bc5a9c15d21c92c
ColtonDx <72266600+ColtonDx@users.noreply.github.com>
2024-10-31 16:32:51 -0500

    Update assign_ticket.sh

--- assign_ticket.sh
+++ assign_ticket.sh
@@ -1,38 +1,31 @@
 #!/bin/bash
 
+# Load configuration
 source /ticket/application/config
 
-# backoff
+
+# Backoff function
 backoff() {
     sleep $backoff
-    backoff=$((backoff / 2))
+    backoff=$((backoff + 2))
 }
 
-# Create the lock file if it doesn't exist
-if [ ! -f "$lockpath" ]; then
-    echo $$ > "$lockpath"
-fi
-
-# Read the PID from the lock file
-lock_pid=$(cat "$lockpath")
-
-# Check if the PID in the lock file matches the current PID
-while [ "$lock_pid" -ne "$$" ] && [ $attempt -le $max_attempts ]; do
-    echo "Server is busy. Trying again shortly."
-    attempt=$((attempt + 1))
-    backoff
-    lock_pid=$(cat "$lockpath")
+while [ $attempt -le $max_attempts ]; do
+    if mkdir "$lockdir"; then
+        echo "Connected to the Server."
+        echo "ASSIGN" > /ticket/application/addaticket
+        if read response < /ticket/application/ticketresponse; then
+           echo $response
+            rmdir "$lockdir"
+            break  # Exit the loop if successful
+        fi
+    else
+        echo "Failed to acquire lock"
+        attempt=$((attempt + 1))
+        backoff
+    fi
 done
 
-if [ "$lock_pid" -eq "$$" ]; then
-    echo "Connected to the Server."
-    echo "ASSIGN" > /ticket/application/addaticket
-    if read response < /ticket/application/ticketresponse; then
-    echo $response
-    rm -rf $lockpath
-fi
-
-else
-    echo "Server Appears to be VERY busy. Please try again later."
-    exit 1
+if [ $attempt -gt $max_attempts ]; then
+    echo "Failed to connect to the server after $max_attempts attempts."
 fi
